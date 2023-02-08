c21c1d31ea7b9883b619f184bc7eb0b2e9ece1ac
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-02-08 08:04:05 +0530

    updates nextjs to next@canary

--- src/components/SeoElement.tsx
+++ src/components/SeoElement.tsx
@@ -8,30 +8,77 @@ export type HeadProps = {
 export default function SeoElement({ ...props }: HeadProps) {
   return (
     <>
-      <link rel="preload" as="image" href={props?.image} />
+      <link
+        rel="preload"
+        as="image"
+        href={props?.image}
+      />
       {/* Primary Meta Tags */}
       <title>{props?.title}</title>
 
-      <meta name="title" content={props?.title} />
-      <meta name="description" content={props?.description} />
+      <meta
+        name="title"
+        content={props?.title}
+      />
+      <meta
+        name="description"
+        content={props?.description}
+      />
 
       {/* Theme */}
       {/* <meta name="theme-color" content="#000" /> */}
 
       {/* Facebook Meta Tags */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={props?.url?.href} />
-      <meta property="og:title" content={props?.title} />
-      <meta property="og:description" content={props?.description} />
-      {props?.image && <meta property="og:image" content={props?.image} />}
+      <meta
+        property="og:type"
+        content="website"
+      />
+      <meta
+        property="og:url"
+        content={props?.url?.href}
+      />
+      <meta
+        property="og:title"
+        content={props?.title}
+      />
+      <meta
+        property="og:description"
+        content={props?.description}
+      />
+      {props?.image && (
+        <meta
+          property="og:image"
+          content={props?.image}
+        />
+      )}
 
       {/* Twitter Meta Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={props?.title} />
-      <meta name="twitter:description" content={props?.description} />
-      {props?.image && <meta name="twitter:image" content={props?.image} />}
-      <meta property="twitter:domain" content={props?.url?.hostname} />
-      <meta property="twitter:url" content={props?.url?.href} />
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+      />
+      <meta
+        name="twitter:title"
+        content={props?.title}
+      />
+      <meta
+        name="twitter:description"
+        content={props?.description}
+      />
+      {props?.image && (
+        <meta
+          name="twitter:image"
+          content={props?.image}
+        />
+      )}
+      <meta
+        property="twitter:domain"
+        content={props?.url?.hostname}
+      />
+      <meta
+        property="twitter:url"
+        content={props?.url?.href}
+      />
     </>
   );
 }
